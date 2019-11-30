239e4522ec6a75cb2bbf4eed2fa7623267f260d8
vtretyak <tretyak1c@gmail.com>
2019-11-30 19:13:35 +0200

    press enter

--- KeepInMind/Views/MainWindow.xaml.cs
+++ KeepInMind/Views/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Dynamic;
+using System.Windows;
+using System.Windows.Input;
 using KeepInMind.ViewModels;
 
 namespace KeepInMind
@@ -10,7 +12,33 @@ namespace KeepInMind
 	{		
 		public MainWindow()
 		{
-			InitializeComponent();			
-		}	
+			InitializeComponent();
+			TextBoxWord.KeyDown += TextBoxWord_KeyDown;
+			TextBoxTranslate.KeyDown += TextBoxTranslate_KeyDown; ;
+		}
+
+		private void TextBoxTranslate_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Key == System.Windows.Input.Key.Enter)
+			{				
+				var vm = (MainViewModel)DataContext;
+				object[] obj = new object[2];
+				obj[0] = TextBoxWord.Text;
+				obj[1] = TextBoxTranslate.Text;
+				
+				if (vm.AddWordCommand.CanExecute(obj))
+				{
+					vm.AddWordCommand.Execute(obj);
+				}
+			}
+		}
+
+		private void TextBoxWord_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+		{
+			if(e.Key == System.Windows.Input.Key.Enter)
+			{
+				FocusManager.SetFocusedElement(this, TextBoxTranslate);				
+			}
+		}
 	}
 }
