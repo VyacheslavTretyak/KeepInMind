9e4f54c6ccd06c968ed21bc5acccf0470a7a1795
vtretyak <tretyak1c@gmail.com>
2020-01-08 16:44:34 +0200

    add try catch

--- KeepInMind/Views/WordWindow.xaml.cs
+++ KeepInMind/Views/WordWindow.xaml.cs
@@ -1,19 +1,8 @@
 ﻿using KeepInMind.Classes;
 using KeepInMind.ViewModels;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace KeepInMind
 {
@@ -52,7 +41,7 @@ namespace KeepInMind
 		}
 		private void CheckButton_Click(object sender, RoutedEventArgs e)
 		{
-			(this.DataContext as WordViewModel).Close();
+			//(this.DataContext as WordViewModel).Close();
 			Close();
 		}
 
